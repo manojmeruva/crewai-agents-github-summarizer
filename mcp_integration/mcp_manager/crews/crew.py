fe427b4846bfbf3e7d020c352a00cd7a2e3936e1
manojmeruva <manojmeruva@gmail.com>
2026-08-19 16:55:55 +0000

    task list , agents added to crewai

--- mcp_integration/mcp_manager/crews/crew.py
+++ mcp_integration/mcp_manager/crews/crew.py
@@ -1,1 +1,15 @@
 from crewai import Crew, Process
+from ..tasks.tasks import analyze_repo_structure_task
+from ..agents.agents import repo_structure_auditor
+
+def build_crew(owner: str, repo: str) -> Crew:
+    tasks = []
+    tasks.extend (analyze_repo_structure_task(owner,repo))
+
+    return Crew(
+        agents = [repo_structure_auditor],
+        tasks = tasks, 
+        process = Process.sequential,
+        verbose = True,
+        cache = True,
+    )
